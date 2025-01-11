02416459abeb6cdb6f2dbef4430c64654a404798
Your Nameclear <you@example.com>
2025-01-11 20:25:13 +0100

    Refactor styles in CSS for improved theming; update particle colors in JS; remove unused image section in HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#000000"
+        "value": "#39ff14"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#000000",
+        "color": "#39ff14",
         "opacity": 0.4,
         "width": 1
       },
